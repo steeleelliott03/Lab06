6cd0dfbc0d0bb3621bffd3df88ec3754fdad0c34
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-03-09 17:01:33 -0500

    encode

---
+++ encoder.py
@@ -0,0 +1,31 @@
+# Steele Elliott
+# Lab 06
+# Software Engineering
+
+def encode(password):
+    new = ''
+    for i in range(0, len(password)):
+        new += chr(int(password[i]) + 3)
+    return password
+
+
+def main():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+
+        choice = int(input('Please enter an option: '))
+        if choice == 1:
+            password = input('Please enter your password to encode: ')
+            encoded = encode(password)
+            print('Your password has been encoded and stored!')
+        elif choice == 2:
+            print(f'The encoded password is {encoded}, and the decoded passworrd is {password}')
+
+
+
+if __name__ == '__main__':
+    main()
